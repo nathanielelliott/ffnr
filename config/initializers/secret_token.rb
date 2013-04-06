ae0faaeda55e559b1d38417d50cb0526fc277393
Nathaniel Elliott <n.elliott@nathanielelliott.com>
2013-04-06 00:41:15 -0400

    Created my first rails system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ffnr::Application.config.secret_token = 'df59cf8b1b3b9295e46684a05864ce8e3b17d75d98d8ab664f5af5e66771d37f4a02d75198a46f0997740ee3488ebfb84fea65d35cda5d88c690cdae344a28fd'
